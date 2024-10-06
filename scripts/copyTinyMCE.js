d4952e5d89afaa91b02b829651cb6fec3d7a82ff
Rod Boev <rod.boev@gmail.com>
2024-10-05 21:06:47 -0400

    Fix postinstall script

--- scripts/copyTinyMCE.js
+++ scripts/copyTinyMCE.js
@@ -1,5 +1,9 @@
-const fs = require('fs-extra')
-const path = require('path')
+import fs from 'fs-extra'
+import path from 'path'
+import { fileURLToPath } from 'url'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
 const source = path.join(__dirname, '..', 'node_modules', 'tinymce')
 const destination = path.join(__dirname, '..', 'src', 'app', 'tinymce')
