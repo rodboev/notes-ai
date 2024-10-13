c0fde40d32087a1c6af38fb9ca30f2f5023c2d22
Rod Boev <rod.boev@gmail.com>
2024-10-12 23:14:24 -0400

    Convert to module

--- scripts/copyTinyMCE.js
+++ scripts/copyTinyMCE.js
@@ -1,5 +1,9 @@
-const fs = require('fs-extra')
-const path = require('path')
+import fs from 'fs-extra'
+import path from 'node:path'
+import { fileURLToPath } from 'node:url'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
 const source = path.join(__dirname, '..', 'node_modules', 'tinymce')
 const destination = path.join(__dirname, '..', 'src', 'app', 'tinymce')
